eb0bc142373483b9f8ea896638a589a2798b7a5c
Nik Silver <nik.silver@gmail.com>
2010-08-08 23:47:41 +0100

    Exploited method further to reduce boilerplate further.

--- src/org/pigsaw/wharfinger/TestingServlet.scala
+++ src/org/pigsaw/wharfinger/TestingServlet.scala
@@ -47,47 +47,36 @@ class TestingServlet extends HttpServlet {
 
       val pm = PMF.get.getPersistenceManager
       val query = pm.newQuery(classOf[Message])
-      try {
+      transaction (query) {
         val message = new Message
         message.setMessage(msg)
         pm.makePersistent(message)
+        resp.getWriter.println("Message was set to '" + msg + "'")
       }
-      catch {
-        case e => resp.getWriter.println("Exception: " + e.getMessage)
-      }
-      finally {
-        query.closeAll
-      }
-
-      resp.getWriter.println("Message was set to '" + msg + "'")
     }
 
     def testLoadMessage: Unit = {
+      resp.setContentType("text/plain")
       val pm = PMF.get.getPersistenceManager
       val query = pm.newQuery(classOf[Message])
-      try {
+      transaction (query) {
         val messages = query.execute.asInstanceOf[java.util.List[Message]]
         val output = messages.size match {
           case 0 => "No message saved"
           case _ => "The message is '" + messages(0).getMessage + "'"
         }
-        resp.setContentType("text/plain")
         resp.getWriter.println(output)
-      }
-      finally {
-        query.closeAll
       }
     }
 
     def testDeleteMessage: Unit = {
       resp.setContentType("text/plain")
-
       val pm = PMF.get.getPersistenceManager
       val query = pm.newQuery(classOf[Message])
       transaction (query) {
         query.deletePersistentAll
+        resp.getWriter.println("Deleted message")
       }
-      resp.getWriter.println("Deleted message")
     }
 
     def transaction(query: javax.jdo.Query)(block: Unit): Unit = {

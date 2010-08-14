58a4185fd589098f04281b897b104c82b4af6736
Nik Silver <nik.silver@gmail.com>
2010-08-14 20:28:51 +0100

    Added forgotten files.

---
+++ src/org/pigsaw/wharfinger/HtmlNode.scala
@@ -0,0 +1,54 @@
+package org.pigsaw.wharfinger
+
+import org.ccil.cowan.tagsoup._
+import xml.{NodeSeq, Node}
+import java.net.{HttpURLConnection, URL}
+import java.io.Reader
+
+/**
+ * Object with a factory method to return an HTML document
+ * from a reader. Use it as
+ * <code>
+ * val html: Node = HtmlNode(new URLReader("http://www.guardian.co.uk"))
+ * </code>
+ */
+
+object HtmlNode {
+
+  def apply(reader: Reader): Node = {
+    val parser = new TagSoupFactoryAdapter
+    return parser load reader
+  }
+}
+
+class URLReader(val url: String)
+        extends java.io.BufferedReader(new java.io.InputStreamReader(new java.net.URL(url).openStream))
+
+object Preamble {
+
+  class RichNodeSeq(ns:Seq[Node]) {
+
+    def containing(nodeBuilder: Node => NodeSeq): Seq[Node] = {
+      ns filter( nodeBuilder(_).length > 0)
+    }
+  }
+
+  implicit def nodeSeq2RichNodeSeq(ns:NodeSeq) = new RichNodeSeq(ns)
+}
+
+/**
+ * Resolve a URL. Create a new instance using a URL string, then the field
+ * <code>URL</code> contains the actual URL after any redirects.
+ * Will not (apparently) resolve across HTTP/HTTPS boundaries; this is a
+ * security feature of Java. See
+ * http://stackoverflow.com/questions/1884230/java-doesnt-follow-redirect-in-urlconnection
+ * for more.
+ */
+class RedirectResolver(url: String) {
+  private val url_obj = new URL(url)
+  private val connection = url_obj.openConnection.asInstanceOf[HttpURLConnection]
+  connection.setRequestMethod("HEAD")
+  connection.connect
+  connection.getInputStream
+  val URL = connection.getURL.toString
+}
---
+++ tests/org/pigsaw/wharfinger/HtmlNodeTest.scala
@@ -0,0 +1,74 @@
+package org.pigsaw.wharfinger
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import Preamble._
+import java.io.{StringReader, IOException}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nik
+ * Date: 07-Aug-2010
+ * Time: 11:01:28
+ * To change this template use File | Settings | File Templates.
+ */
+
+class HtmlNodeTest extends Spec with ShouldMatchers {
+
+  describe("SimpleHtmlHandler") {
+
+    it("Should read HTML text") {
+      val str = "<a>This is the first level<b>This is the second level</b></a>"
+      val html = HtmlNode(new StringReader(str));
+      val message = (html \\ "b").text
+      message should be ("This is the second level")
+    }
+
+    it("Should be able to use 'containing' to limit to nodes which contain certain other node sequences") {
+      val html = HtmlNode(new StringReader(Data.instapaper_html))
+      val nodes = html \\ "li" containing (_ \\ "@accesskey")
+      
+      nodes should have length (3)
+
+      nodes(0).label should be ("li")
+      nodes(0).text should be ("Skip past navigation")
+      nodes(1).label should be ("li")
+      nodes(1).text should be ("Skip to navigation")
+      nodes(2).label should be ("li")
+    }
+
+    it("Should read HTML from a URL") {
+      val html = HtmlNode(new URLReader("http://www.google.com"))
+      val title = (html \\ "title").text
+      title should be ("Google")
+    }
+  }
+
+  it("Should handle reading from a problematic URL") {
+    def readURL = HtmlNode(new URLReader("http://www.willnotresolve5432.com"))
+    evaluating(readURL) should produce [IOException]
+  }
+
+  it("Should allow URL redirects to be resolved") {
+    val resolver = new RedirectResolver("http://bit.ly/9NQcyA")
+    resolver.URL should be ("http://www.mobileuserexperience.com/?p=896")
+  }
+
+  it("Should throw an IOException if URL redirect cannot be resolved") {
+    evaluating(new RedirectResolver("http://www.madeupdomain54321.com")) should produce [IOException]
+  }
+
+  /* This will not work due to Java's security.
+   *
+  it("Should resolve URLs across HTTP(S) boundaries") {
+    val resolver = new RedirectResolver("http://bit.ly/3hQYj")
+    resolver.URL should be ("https://www.google.com")
+  }
+  */
+
+  it("Should read HTML from a redirected URL") {
+    val html = HtmlNode(new URLReader("http://bit.ly/9NQcyA"))
+    val title = (html \\ "title").text
+    title should include ("A mobile developer day too far")
+  }
+}

e492e14bcc424f73a85473efbeb57adbc3d13a92
Nik Silver <nik.silver@gmail.com>
2010-08-15 00:37:16 +0100

    Trying to pull back content via Instapaper.

---
+++ tests/org/pigsaw/wharfinger/InstapaperHandlerTest.scala
@@ -0,0 +1,31 @@
+package org.pigsaw.wharfinger
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import xml.Node
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nik
+ * Date: 14-Aug-2010
+ * Time: 23:43:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+class InstapaperHandlerTest extends Spec with ShouldMatchers {
+
+  describe("InstapaperHandler") {
+
+    it("Should create an appropriate URL") {
+      val handler = new InstapaperHandler("http://online.wsj.com/article/SB10001424052748703977004575393173432219064.html")
+      handler.url should be ("http://www.instapaper.com/text?u=http%3A%2F%2Fonline.wsj.com%2Farticle%2FSB10001424052748703977004575393173432219064.html")
+    }
+
+    it("Should be able to find the content div") {
+      val handler = new InstapaperHandler("http://online.wsj.com/article/SB10001424052748703977004575393173432219064.html")
+      val content: Node = handler.getContentDiv()
+      val h1 = content \\ "h1"
+      h1.text should be ("Sites Feed Personal Details To New Tracking Industry")
+    }
+  }
+}

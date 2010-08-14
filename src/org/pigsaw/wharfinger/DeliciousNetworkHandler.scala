70450b240068e86f7cecae50978bba02d7761a99
Nik Silver <nik.silver@gmail.com>
2010-08-14 19:28:32 +0100

    More refactoring.

--- src/org/pigsaw/wharfinger/DeliciousNetworkHandler.scala
+++ src/org/pigsaw/wharfinger/DeliciousNetworkHandler.scala
@@ -14,7 +14,9 @@ class DeliciousNetworkHandler(val reader: Reader) {
   val bookmarks = new ListBuffer[ArticleURL]()
 
   def this() = this(new UrlReader("http://delicious.com/network/nik.silver?setcount=100"))
-  
+
+  /**Parse the HTML to create the bookmarks.
+   */
   def parse() {
     val html = HtmlNode(reader)
     val bookmarks_div =  html \\ "div" having (_ \ "@class" filter (_.text contains "bookmark "))
@@ -22,16 +24,18 @@ class DeliciousNetworkHandler(val reader: Reader) {
       val link = bookmark \\ "a" having (_ \ "@class" filter (_.text contains "taggedlink"))
       val article_url = new ArticleURL((link \ "@href").text)
       bookmarks += article_url
-      process(article_url)
     }
   }
 
-  /** Process a single bookmark. */
-  def process(bookmark: ArticleURL) {
-    val task = new QueueableTask("/fetchurl")
-    task += ("url" -> bookmark.url)
-    task.enqueue
-  }
 }
 
-class ArticleURL(val url: String)
+class ArticleURL(val url: String) {
+
+  /** Process a single bookmark. */
+  def process() {
+    val task = new QueueableTask("/fetchurl")
+    task += ("url" -> url)
+    task.enqueue
+  }
+
+}
--- tests/org/pigsaw/wharfinger/DeliciousNetworkHandlerTest.scala
+++ tests/org/pigsaw/wharfinger/DeliciousNetworkHandlerTest.scala
@@ -20,27 +20,12 @@ class DeliciousNetworkHandlerTest extends Spec with ShouldMatchers {
   describe("DeliciousNetworkHandler") {
 
     it("Should return a list of bookmarks") {
-      val handler = new DeliciousNetworkHandler(new StringReader(Data.delicious_html)) {
-        override def process(b: ArticleURL) {}
-      }
+      val handler = new DeliciousNetworkHandler(new StringReader(Data.delicious_html))
       handler.parse
       handler.bookmarks.size should be === (10)
       val bookmark0 = handler.bookmarks(0)
       bookmark0.url should be ("http://www.leedsunited-mad.co.uk/news/tmnw/leeds_40_lincoln_by_sean_markey_aged_11_545231/index.shtml")
     }
 
-    it("Should process each bookmark") {
-      val processed_bookmarks = new ListBuffer[ArticleURL]()
-      val handler = new DeliciousNetworkHandler(new StringReader(Data.delicious_html)) {
-        override def process(bookmark: ArticleURL) {
-          processed_bookmarks += bookmark
-        }
-      }
-      handler.parse
-
-      processed_bookmarks.size should be === (10)
-      processed_bookmarks(0).url should be ("http://www.leedsunited-mad.co.uk/news/tmnw/leeds_40_lincoln_by_sean_markey_aged_11_545231/index.shtml")
-      processed_bookmarks(9).url should be ("http://labs.slate.com/blog/2010/08/welcome-to-slate-labs/")
-    }
   }
 }

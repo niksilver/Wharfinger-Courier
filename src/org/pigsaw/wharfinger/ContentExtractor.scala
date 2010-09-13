385d69af2b9d9a1a9fcbcf2e745e684973d42c32
Nik Silver <nik.silver@gmail.com>
2010-09-13 23:21:03 +0100

    Sketching out the Instapaper replacement.

--- src/org/pigsaw/wharfinger/ContentExtractor.scala
+++ src/org/pigsaw/wharfinger/ContentExtractor.scala
@@ -8,13 +8,32 @@ import xml.Node
  * http://code.google.com/p/arc90labs-readability/source/browse/trunk/js/readability.js?r=162
  */
 
-class ContentExtractor(val node: Node) {
+class ContentExtractor(val html: Node) {
 
-  val article_title: Node = getArticleTitle
-  val article_content: Node = grabArticle
-  def removeScripts: Node = { null }
-  def findNextPageLink: String = { null }
-  def prepDocument: Node = { null }
-  def getArticleTitle: Node = { null }
+  // Right now we won't deal with:
+  // - Next page links
+  // - Frames
+
+  val html1 = removeScripts(html)
+  val html2 = prepDocument(html1)
+  val article_title: String = getArticleTitle(html2)
+  val article_content: Node = grabArticle(html2)
+
+  def removeScripts(html: Node): Node = { null }
+  def getArticleTitle: Node = {
+    // 273-280: Get document title from the first tag called <title>
+    // Now tidy the title...
+    // 282-289: If the title has ' - ' or ' | ' then get the text before that, unless it's
+    //          less than three words, in which case take what's after it.
+    // 290-297: Else if title has ': ' then get the text before that, unless it's
+    //          less than three words, in which case take what's after it.
+    // 298-305: Else if the title is too big (>150 chars) or too small (<15 chars) and
+    //          there's only one H1, then take the text of the H1
+    // Trim what we've got, and if it's less than 4 words then go back to what we
+    // got originally
+  }
+  def prepDocument: Node = {
+    // 358: WE ARE HERE
+  }
   def grabArticle: Node = { null }
 }

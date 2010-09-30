52d39be8557f4e25d6fe13e0705bdbb9e56187cd
Nik Silver <nik.silver@gmail.com>
2010-10-01 00:07:39 +0100

    Should now have a simple constructor that takes a trait.

--- src/org/pigsaw/wharfinger/URLString.scala
+++ src/org/pigsaw/wharfinger/URLString.scala
@@ -11,36 +11,32 @@ package org.pigsaw.wharfinger
  *   /some/page.html?q=z
  */
 
-class URLString private (val server: String, val path: String) {
+class URLString (url: String) {
 
-  def this(url: String) = this(url)
+  // Separate
+  //   http(s)://domain.name:1234
+  // from
+  //   /some/page.html?q=z
+  private val server_re = """(https?:\/\/[-A-Za-z.:0-9]*)""".r
+
+  val (server, path) = server_re.findFirstIn(url) match {
+    case Some(s) => (s, url.drop(s.length))
+    case None => ("", url)
+  }
 
   def isBad = (server == "")
 
   def normalise = new URLString(
-    server.toLowerCase,
-    path match {
+    server.toLowerCase +
+    (path match {
       case "/" => ""
       case _ => path
-    })
+    }))
 
   override def toString = server + path
 }
 
 object URLString {
-  // Separate
-  //   http(s)://domain.name:1234
-  // from
-  //   /some/page.html?q=z
-  private val separator = """(https?:\/\/[-A-Za-z.:0-9]*)(.*)""".r
 
-  def apply(url: String) = {
-    try {
-      val separator(server, path) = url
-      new URLString(server, path)
-    }
-    catch {
-      case _ => new URLString("", url)
-    }
-  }
+  def apply(url: String) = new URLString(url)
 }

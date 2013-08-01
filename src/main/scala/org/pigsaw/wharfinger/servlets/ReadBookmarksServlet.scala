f2410458981c29a3855a604354406c095cff8bd3
Nik Silver <nik@niksilver.com>
2013-08-01 20:27:45 +0100

    We now have a rather ugly extraction of logging from the ReadBookmarksServlet.

--- src/main/scala/org/pigsaw/wharfinger/servlets/ReadBookmarksServlet.scala
+++ src/main/scala/org/pigsaw/wharfinger/servlets/ReadBookmarksServlet.scala
@@ -8,31 +8,55 @@ import org.pigsaw.wharfinger.{BookmarkCollator, BookmarkPendingFetch, URLString,
  * Read some bookmarks.
  */
 
-class ReadBookmarksServlet extends HttpServlet {
+class ReadBookmarksServlet extends ReadBookmarksServletShell with ServletLogger {
+  val oLogger = Some(Logger.getLogger(this.getClass.getName))
+  val oResp = None
+}
 
-  val log = Logger.getLogger(this.getClass.getName)
+trait ReadBookmarksServletShell extends HttpServlet {
+  this: ServletLogger =>
 
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
     resp.setContentType("text/plain")
 
-    lazy val handler = req.getPathInfo.tail.split('/')(0) match {
-      case "twitter-times" => new TwitterTimesCollator
-      case service => new UnknownServiceHandler(service, log)
-    }
+    lazy val handler = getHandler(req.getPathInfo, this.withResponse(resp))
     handler.bookmarks
-    handler.bookmarksPendingFetch.foreach( b => {
+    handler.bookmarksPendingFetch map { b: BookmarkPendingFetch =>
       resp.getWriter.println("Saving for later fetching: " + b.url)
       val normalised_url = URLString(b.url).normalise.toString
       new BookmarkPendingFetch(normalised_url, b.title, b.getCitation).saveForLaterFetching()
-    })
-
-    class UnknownServiceHandler(service: String, log: Logger) extends BookmarkCollator[Nothing] {
-      def bookmarks = {
-        resp.getWriter.println("Unknown bookmark service: " + service)
-        log.warning("Unknown bookmark service: " + service)
-        Seq()
-      }
-      def bookmarksPendingFetch = Nil
     }
   }
+
+  def getHandler(pathInfo: String, sLogger: ServletLogger) =
+    pathInfo.tail.split('/')(0) match {
+      case "twitter-times" => new TwitterTimesCollator
+      case service => new UnknownServiceHandler(service, sLogger)
+    }
+}
+
+class UnknownServiceHandler(service: String, logger: ServletLogger) extends BookmarkCollator[Nothing] {
+  def bookmarks = {
+    logger.warn("Unknown bookmark service: " + service)
+    Seq()
+  }
+  def bookmarksPendingFetch = Nil
+}
+
+trait ServletLogger {
+  outer: ServletLogger =>
+
+  val oLogger: Option[Logger]
+  val oResp: Option[HttpServletResponse]
+
+  def warn(msg: String) {
+    oResp map { r => r.getWriter.println(msg) }
+    oLogger map { g => g.warning(msg)}
+  }
+
+  def withResponse(resp: HttpServletResponse): ServletLogger =
+    new ServletLogger {
+      val oLogger = outer.oLogger
+      val oResp = Some(resp)
+    }
 }

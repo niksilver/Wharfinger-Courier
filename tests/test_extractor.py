a0a4b0660e2f3725311903a62398f45f6c7f2a36
Nik Silver <nik@niksilver.com>
2026-04-18 09:40:05 +0100

    Test article extraction returns valid XML.

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,5 +1,7 @@
 """Tests covering article extraction."""
 
+import lxml
+
 from tests.conftest import sample_html
 
 import courier
@@ -10,3 +12,9 @@ def test_extract_article_returns_tuple(sample_html):
     (title, xhtml) = extract_article(sample_html)
     assert len(title) > 0
     assert len(xhtml) > 0
+
+
+def test_extract_article_xhtml_is_valid_xml(sample_html):
+    (title, xhtml) = extract_article(sample_html)
+    # Should not throw an error
+    lxml.etree.fromstring(xhtml)
